5e7754c886df0d063460e4f728d307304eabde2c
SgaBenza <sgabenza86@gmail.com>
2017-09-25 00:43:22 +0200

    Handling Error - Expected | Unexpected | Bad Request

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,3 +1,6 @@
+import { BadInput } from '../common/bad-input';
+import { NotFoundError } from '../common/not-found-error';
+import { AppError } from '../common/app-error';
 import { PostsService } from '../services/posts.service';
 import { subscribeOn } from 'rxjs/operator/subscribeOn';
 import { Component, OnInit } from '@angular/core';
@@ -10,7 +13,6 @@ import { Component, OnInit } from '@angular/core';
 export class PostsComponent implements OnInit {
   posts: any[];
 
-
   constructor(private service: PostsService) {
   }
 
@@ -18,32 +20,61 @@ export class PostsComponent implements OnInit {
     let post = { title: input.value }
     input.value = '';
     this.service.createPost(post)
-    .subscribe(response => {
-      post['id'] = response.json().id;
-      this.posts.splice(0, 0, post);
-      console.log(response.json());
-    });
+      .subscribe(
+      response => {
+        post['id'] = response.json().id;
+        this.posts.splice(0, 0, post);
+      },
+      // (error: Response) => {
+        (error: AppError) => {
+        if (error instanceof BadInput){
+          // this.form.setErrors(error.json());
+
+          // this.form.setErrors(error.originalError); GIUSTA
+        }
+        else {
+          alert('An unexpected error occurred.');
+          console.log(error);
+        }
+      });
   }
 
-   updatePost(post) {
+  updatePost(post) {
     this.service.updatePost(post)
       .subscribe(response => {
         console.log(response.json());
-      })
+      }, error => {
+        alert('An unexpected error occurred.');
+        console.log(error);
+      });
   }
 
   deletePost(post) {
-    this.service.deletePost(post.id)
-      .subscribe(response => {
+    this.service.deletePost(345)
+      .subscribe(
+      response => {
         let index = this.posts.indexOf(post);
         this.posts.splice(index, 1);
-      })
-  } 
+      },
+      // (error: Response) => {
+        (error: AppError) => {
+        // if (error.status === 404)
+        if( error instanceof NotFoundError)
+          alert('This post has already been deleted');
+        else {
+          alert('An unexpected error occurred.');
+          console.log(error);
+        }
+      });
+  }
 
   ngOnInit() {
     this.service.getPosts()
       .subscribe(response => {
         this.posts = response.json();
+      }, error => {
+        alert('An unexpected error occurred.');
+        console.log(error);
       });
   }
 
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,5 +1,12 @@
+import { NotFoundError } from '../common/not-found-error';
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+import { AppError } from '../common/app-error';
+import { BadInput } from '../common/bad-input';
 
 @Injectable()
 export class PostsService {
@@ -7,20 +14,32 @@ export class PostsService {
 
   constructor(private http: Http) { }
 
-  getPosts(){
-   return this.http.get(this.url);
+  getPosts() {
+    return this.http.get(this.url);
   }
 
-  createPost(post){
-   return this.http.post(this.url, JSON.stringify(post));
+  createPost(post) {
+    return this.http.post(this.url, JSON.stringify(post))
+      .catch((error: Response) => {
+        if (error.status === 400)
+          return Observable.throw(new BadInput(error.json()))
+
+        return Observable.throw(new AppError(error.json()))  
+      });
   }
 
-  updatePost(post){
+  updatePost(post) {
     return this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }));
   }
 
-  deletePost(id){
+  deletePost(id) {
     return this.http.delete(this.url + '/' + id)
+      .catch((error: Response) => {
+        if (error.status === 404)
+          return Observable.throw(new NotFoundError());
+
+        return Observable.throw(new AppError(error));
+      });
   }
 
 }
